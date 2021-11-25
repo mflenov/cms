3140a713a48f731c3cf96c27118095936fb1bebd
mikhail flenov <flenov@example.com>
2021-11-25 17:44:02 -0500

    updated filter save API to use the FCmsManager rather than test data

--- FCmsManagerAngular/ViewModels/FilterViewModel.cs
+++ FCmsManagerAngular/ViewModels/FilterViewModel.cs
@@ -1,28 +1,50 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using FCms.Content;
 
 namespace FCmsManagerAngular.ViewModels
 {
     public class FilterViewModel
     {
+        public FilterViewModel()  {
+        }
+
+        public FilterViewModel(IFilter filter)  {
+            Id = filter.Id;
+            Name = filter.Name;
+            Type = filter.Type;
+        }
+
         public Guid? Id { get; set; }
 
         public string Name { get; set; }
 
         public string Type { get; set; }
+        
+        public List<string> Values { get; set; }
 
+        public void Add(ICmsManager manager) {
+            IFilter filtermodel = FCms.Factory.FilterFactory.CreateFilterByType((IFilter.FilterType)Enum.Parse(typeof(IFilter.FilterType), Type));
+            manager.Data.Filters.Add(MapToModel(filtermodel));
+            manager.Save(); 
+        }
 
-        public static IEnumerable<FilterViewModel> MapFromCmaFilter(List<FCms.Content.IFilter> filters)
+        public void Update(ICmsManager manager) {
+            int repoindex = manager.Data.Filters.Select((v, i) => new { filter = v, Index = i }).FirstOrDefault(x => x.filter.Id == Id)?.Index ?? -1;
+            if (repoindex < 0)
+            {
+                throw new Exception("The filter definition not found");
+            }
+            this.MapToModel(manager.Data.Filters[repoindex]);
+            manager.Save();
+        }
+
+        public IFilter MapToModel(IFilter model)
         {
-            return filters.Select(m =>
-                new FilterViewModel()
-                {
-                    Id = m.Id,
-                    Name = m.Name,
-                    Type = m.Type
-                }
-           );
+            model.Name = this.Name;
+            model.Id = this.Id ?? Guid.NewGuid();
+            return model;
         }
     }
 }
