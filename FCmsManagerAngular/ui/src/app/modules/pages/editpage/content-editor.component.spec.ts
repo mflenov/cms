1ad460887dfbc98257f0c800f9b2f613265b376c
mflenov <mikhail@flenov>
2022-01-26 16:35:02 +0300

    add dynamic content editor

---
+++ FCmsManagerAngular/ui/src/app/modules/pages/editpage/content-editor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContentEditorComponent } from './content-editor.component';
+
+describe('ContentEditorComponent', () => {
+  let component: ContentEditorComponent;
+  let fixture: ComponentFixture<ContentEditorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContentEditorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContentEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ FCmsManagerAngular/ui/src/app/modules/pages/editpage/content-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-editor',
+  templateUrl: './content-editor.component.html',
+  styleUrls: ['./content-editor.component.css']
+})
+export class ContentEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
