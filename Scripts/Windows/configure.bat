e1567aa134ef5a289c495c4ddaa6a9798c6cebcb
Mikhail Flenov <Mikhail.Flenov@dayforce.com>
2025-02-12 15:48:30 -0500

    fix build - downgrade angular

--- Scripts/Windows/configure.bat
+++ Scripts/Windows/configure.bat
@@ -1,15 +1,15 @@
 :: install Microsoft.DotNet.SDK.8
-winget install Microsoft.DotNet.SDK.8
+winget install --silent --accept-source-agreements Microsoft.DotNet.SDK.8
 
 :: install docker
-winget install Docker.DockerDesktop
-winget install Docker.DockerCLI
+winget install --silent --accept-source-agreements Docker.DockerDesktop
+winget install --silent --accept-source-agreements Docker.DockerCLI
 
 :: add pgdatabase
 docker run -p 5432:5432 --name fcmspgdb -e POSTGRES_PASSWORD=password -e POSTGRES_USER=postgres -e POSTGRES_DB=fcms -d postgres
 
 :: NodeJS
-winget install OpenJS.NodeJS
+winget install --silent --accept-source-agreements OpenJS.NodeJS -v 20.0.0
 
 :: typescript
 winget install Microsoft.VisualStudio.Extensions.TypeScript
