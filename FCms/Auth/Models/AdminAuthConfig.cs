d57f24e274ae450e471b400f7243458467dd49fc
mflenov <m@flenov>
2026-03-02 19:14:03 -0500

    refactor hash password

--- FCms/Auth/Models/AdminAuthConfig.cs
+++ FCms/Auth/Models/AdminAuthConfig.cs
@@ -10,44 +10,28 @@ public class CmsUserModel
     public string Username { get; set; }
     public string PasswordHash {get; set;}
     
-    public string HashPassword(string password)
+    private string ComputeHash(string password, byte[] salt)
     {
-        byte[] salt = RandomNumberGenerator.GetBytes(16);
-        
         byte[] hash = KeyDerivation.Pbkdf2(
             password: password,
             salt: salt,
             prf: KeyDerivationPrf.HMACSHA256,
             iterationCount: 100000,
             numBytesRequested: 32);
-        
-        // Combine salt and hash: first 16 bytes are salt, remaining 32 bytes are hash
+
         byte[] hashBytes = new byte[48];
         Array.Copy(salt, 0, hashBytes, 0, 16);
         Array.Copy(hash, 0, hashBytes, 16, 32);
-        
+
         return Convert.ToBase64String(hashBytes);
     }
 
+    public string HashPassword(string password)
+         => ComputeHash(password, RandomNumberGenerator.GetBytes(16));
+
     public bool VerifyPassword(string password)
     {
-        byte[] hashBytes = Convert.FromBase64String(PasswordHash);
-
-        byte[] salt = new byte[16];
-        Array.Copy(hashBytes, 0, salt, 0, 16);
-
-        byte[] hash = KeyDerivation.Pbkdf2(
-            password: password,
-            salt: salt,
-            prf: KeyDerivationPrf.HMACSHA256,
-            iterationCount: 100000,
-            numBytesRequested: 32);
-
-        for (int i = 0; i < 32; i++)
-        {
-            if (hashBytes[i + 16] != hash[i])
-                return false;
-        }
-        return true;
+        byte[] salt = Convert.FromBase64String(PasswordHash).AsSpan(0, 16).ToArray();
+        return ComputeHash(password, salt) == PasswordHash;
     }
 }
